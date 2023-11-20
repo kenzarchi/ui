ea348f07be4c428c4491738ceb2bf722543dceb0
kenzarchi <kenzarchi2@gmail.com>
2023-11-20 02:39:58 +0100

    Update columns.tsx

--- apps/www/app/examples/tasks/components/columns.tsx
+++ apps/www/app/examples/tasks/components/columns.tsx
@@ -1,14 +1,6 @@
-"use client"
-
-import { ColumnDef } from "@tanstack/react-table"
-
-import { Badge } from "@/registry/new-york/ui/badge"
-import { Checkbox } from "@/registry/new-york/ui/checkbox"
-
-import { labels, priorities, statuses } from "../data/data"
-import { Task } from "../data/schema"
-import { DataTableColumnHeader } from "./data-table-column-header"
-import { DataTableRowActions } from "./data-table-row-actions"
+import { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "@/registry/new-york/ui/checkbox";
+import { DataTableColumnHeader } from "./data-table-column-header";
 
 export const columns: ColumnDef<Task>[] = [
   {
@@ -35,89 +27,48 @@ export const columns: ColumnDef<Task>[] = [
     enableSorting: false,
     enableHiding: false,
   },
+  // ... other existing columns like 'id', 'title', 'status', etc. ...
   {
-    accessorKey: "id",
+    accessorKey: "student",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Task" />
+      <DataTableColumnHeader column={column} title="Student" />
     ),
-    cell: ({ row }) => <div className="w-[80px]">{row.getValue("id")}</div>,
-    enableSorting: false,
-    enableHiding: false,
+    cell: ({ row }) => <div>{row.getValue("student")}</div>,
   },
   {
-    accessorKey: "title",
+    accessorKey: "year",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Title" />
+      <DataTableColumnHeader column={column} title="Year" />
     ),
-    cell: ({ row }) => {
-      const label = labels.find((label) => label.value === row.original.label)
-
-      return (
-        <div className="flex space-x-2">
-          {label && <Badge variant="outline">{label.label}</Badge>}
-          <span className="max-w-[500px] truncate font-medium">
-            {row.getValue("title")}
-          </span>
-        </div>
-      )
-    },
+    cell: ({ row }) => <div>{row.getValue("year")}</div>,
   },
   {
-    accessorKey: "status",
+    accessorKey: "semester",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Status" />
+      <DataTableColumnHeader column={column} title="Semester" />
     ),
-    cell: ({ row }) => {
-      const status = statuses.find(
-        (status) => status.value === row.getValue("status")
-      )
-
-      if (!status) {
-        return null
-      }
-
-      return (
-        <div className="flex w-[100px] items-center">
-          {status.icon && (
-            <status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-          )}
-          <span>{status.label}</span>
-        </div>
-      )
-    },
-    filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id))
-    },
+    cell: ({ row }) => <div>{row.getValue("semester")}</div>,
   },
   {
-    accessorKey: "priority",
+    accessorKey: "supervisor",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Priority" />
+      <DataTableColumnHeader column={column} title="Supervisor" />
     ),
-    cell: ({ row }) => {
-      const priority = priorities.find(
-        (priority) => priority.value === row.getValue("priority")
-      )
-
-      if (!priority) {
-        return null
-      }
-
-      return (
-        <div className="flex items-center">
-          {priority.icon && (
-            <priority.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-          )}
-          <span>{priority.label}</span>
-        </div>
-      )
-    },
-    filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id))
-    },
+    cell: ({ row }) => <div>{row.getValue("supervisor")}</div>,
   },
   {
-    id: "actions",
-    cell: ({ row }) => <DataTableRowActions row={row} />,
+    accessorKey: "keyWord",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Key Word" />
+    ),
+    cell: ({ row }) => <div>{row.getValue("keyWord")}</div>,
   },
-]
+  {
+    accessorKey: "rating",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Rating" />
+    ),
+    cell: ({ row }) => <div>{row.getValue("rating")}</div>,
+  },
+  // ... any additional columns ...
+];
